1e8bc7229922d8290f88713fea6de9c5e960735a
dasireddy26 <dasireddy26@gmail.com>
2019-04-27 19:02:36 +0530

    changes happen in variable concepts

--- variable_values.sh
+++ variable_values.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 x=45
-echo "$x"
-echo "$z"
-z=67
-echo "$z"
+y=56
+z=89
+p=90
+
+<< hai
+echo "the value of x is: $x"
+echo "the value of y is: $y"
+echo "the value of z is: $z"
+echo "the value of p is: $p"
+hai
+
+echo -e  "the value of x is: $x \n the value of y is: $y \n the value of z is: $z \n the value of p is: $p" 
+
