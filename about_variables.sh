4d6bc2c85d90629c2ff6ddd45c2eae34da9ba56b
dasireddy26 <dasireddy26@gmail.com>
2019-04-27 18:37:56 +0530

    variable concepts

---
+++ about_variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+x=23
+y=12
+z=56
+
+echo "Hello, welcome to variable concepts"
+echo "$x"
+echo $y
+echo $z
+#ls
+#pwd
---
+++ variable_values.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+x=45
+echo "$x"
+echo "$z"
+z=67
+echo "$z"
