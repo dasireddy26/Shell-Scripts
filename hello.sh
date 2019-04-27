805748e9a36a2bbe41f7db9aa708c30b314e32da
dasireddy26 <dasireddy26@gmail.com>
2019-04-27 18:22:17 +0530

    write hello.sh script

---
+++ hello.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Hello, welcome to shell scripting courcse"
+echo 'This message is using single quotes'
+echo hi dasireddy
+
+
+echo "=========This command used for double quotes==========="
+echo "$(ls)"
+
+
+echo "===============This is used for single quotes=============="
+echo '$(ls)'
+
+
+echo "=========This is used for without quotes====================="
+echo $(ls)
